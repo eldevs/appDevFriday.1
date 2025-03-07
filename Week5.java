1b350fe913d0f3ecc5f7a053e8c1c0e59abe83b6
elsibulo <esibulo@gmail.com>
2025-03-07 20:00:23 +0800

    Added Sibulo.java with age calculation and updated week5.java

---
+++ Week5.java
@@ -0,0 +1,8 @@
+public class Week5 {
+    public static void main(String[] args) {
+        System.out.println("Collaborative Java Project\n");
+        // Call each student's method
+       // Add your method below
+        Sibulo.display(); 
+    }
+}
