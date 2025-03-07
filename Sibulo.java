ababd53fa0ad3a5fd60e3291c86f343493964a08
juntabios <juntabios@yahoo.com>
2025-03-07 20:40:44 +0800

    modified Sibulo.java with age calculation and updated Week5.java

--- Sibulo.java
+++ Sibulo.java
@@ -1,6 +1,6 @@
 public class Sibulo {
     public static void display() {
-        int age = 20; // Replace with user input if necessary
+        int age = 21; // Replace with user input if necessary
         System.out.println("Hello! This is Sibulo contributing to the project.");
         System.out.println("My current age is: " + age);
         System.out.println("Next year, I will be: " + (age + 1));
